f483b1a8402a75a6f65572a20abcb31e23fb3937
Aras Rasouli <a.rasouli@gmail.com>
2025-02-15 19:45:06 +0100

    chore: configure app to apply feature and changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
 
   routeRules: {
-    '/': { redirect: '/Home' },
+    "/": { redirect: "/Home" },
   },
 
-  compatibilityDate: '2025-02-12',
+  compatibilityDate: "2025-02-12",
 
   runtimeConfig: {
     public: {
@@ -13,7 +13,21 @@ export default defineNuxtConfig({
       AZURE_AUTHORITY: process.env.NUXT_PUBLIC_AZURE_AUTHORITY,
       AZURE_REDIRECT_URI: process.env.NUXT_PUBLIC_AZURE_REDIRECT_URI,
       AZURE_SECRET_VALUE: process.env.NUXT_PUBLIC_AZURE_SECRET_VALUE,
+      AZURE_STORAGE_ACCOUNT_NAME: process.env.NUXT_PUBLIC_AZURE_STORAGE_ACCOUNT_NAME,
+      AZURE_SAS_TOKEN: process.env.NUXT_PUBLIC_AZURE_SAS_TOKEN,
+      AZURE_CONTAINER_NAME: process.env.NUXT_PUBLIC_AZURE_CONTAINER_NAME,
+      AZURE_USE_EMULATOR: process.env.NUXT_PUBLIC_USE_EMULATOR,
     },
   },
-  plugins: ["~/plugins/msal.ts"],
+  plugins: ["~/plugins/msal.ts", "~/plugins/buffer-polyfill.ts"],
+  build: {
+    extend(config, { isClient }) {
+      if (isClient) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          buffer: require.resolve("buffer/"),
+        };
+      }
+    },
+  },
 });

8ecb7c838ce383b5c91b616094907030124b2f32
Aras Rasouli <a.rasouli@gmail.com>
2025-02-18 04:11:58 +0100

    chore: update configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,8 @@ export default defineNuxtConfig({
       AZURE_SAS_TOKEN: process.env.NUXT_PUBLIC_AZURE_SAS_TOKEN,
       AZURE_CONTAINER_NAME: process.env.NUXT_PUBLIC_AZURE_CONTAINER_NAME,
       AZURE_USE_EMULATOR: process.env.NUXT_PUBLIC_USE_EMULATOR,
+      AZURE_SERVICE_BUS_CONNECTION_STRING: process.env.NUXT_PUBLIC_AZURE_SERVICE_BUS_CONNECTION_STRING,
+      AZURE_SERVICE_BUS_QUEUE_NAME: process.env.NUXT_PUBLIC_AZURE_SERVICE_BUS_QUEUE_NAME,
     },
   },
   plugins: ["~/plugins/msal.ts", "~/plugins/buffer-polyfill.ts"],
@@ -26,6 +28,7 @@ export default defineNuxtConfig({
         config.resolve.fallback = {
           ...config.resolve.fallback,
           buffer: require.resolve("buffer/"),
+          process: require.resolve("process/"),
         };
       }
     },
--- plugins/buffer-polyfill.ts
+++ plugins/buffer-polyfill.ts
@@ -1,6 +1,8 @@
 import { defineNuxtPlugin } from '#app';
 import { Buffer } from 'buffer';
+import process from "process";
 
 export default defineNuxtPlugin(() => {
   globalThis.Buffer = Buffer;
+  globalThis.process = process;
 });
